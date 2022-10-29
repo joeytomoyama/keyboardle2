d12379b2734b07526394ab994847260a0503faa7
Joey Prüssing <j.pruessing28@gmail.com>
2022-10-29 21:33:54 +0200

    percussion now pointer and added helpScreen animation

--- misc/test/test.js
+++ misc/test/test.js
@@ -1,10 +1,13 @@
 console.log('test')
+const var12 = new Audio('../../notes/4c.mp3')
 
 const key = document.querySelector('.key')
 
 key.addEventListener('mousedown', (e) => {
     key.style.background = 'white'
     key.style.color = 'black'
+    var12.currentTime = 0
+    var12.play()
 })
 
 key.addEventListener('mouseup', (e) => {
